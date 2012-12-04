7e86ee6cb1f7335bf048578b88f24f159581292b
Nathaniel Bomberger <nathaniel.bomberger@gmail.com>
2012-12-04 15:48:18 -0500

    Updated project files to

--- Todoly/TBAppDelegate.m
+++ Todoly/TBAppDelegate.m
@@ -15,8 +15,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Set the API credentials to use throughout the application
     CMAPICredentials *credentials = [CMAPICredentials sharedInstance];
-    credentials.appIdentifier = @"84e5c4a381e7424b8df62e055f0b69db";
-    credentials.appSecret = @"84c8c3f1223b4710b180d181cd6fb1df";
+    credentials.appIdentifier = @"ec2c161f7a4b485981230a7b0a28f3fe";
+    credentials.appSecret = @"e1767a2e93824f219d75969ee64cbff1";
 
     return YES;
 }
